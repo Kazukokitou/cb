d73ca36ce14bc67d5dd8731fffad727015fcc89f
Junichi Ito <jit@sonicgarden.jp>
2012-12-31 23:15:10 +0900

    Refactoring Sass / Add fb link to nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+# coding: utf-8
 module ApplicationHelper
+  def fb_link
+    "☞ #{fb_icon}".html_safe
+  end
+
+  def fb_icon
+    link_to image_tag('fb.png'),  "http://www.facebook.com/coupebaguette", target: '_blank'
+  end
 end
