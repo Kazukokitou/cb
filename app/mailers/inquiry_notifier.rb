ed8a3198fb73702991a21226784356aff9676423
Junichi Ito <jit@sonicgarden.jp>
2013-01-03 05:41:03 +0900

    Refactoring

--- app/mailers/inquiry_notifier.rb
+++ app/mailers/inquiry_notifier.rb
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
 class InquiryNotifier < ActionMailer::Base
-  default :from => ENV['CB_MAIL_FR']
+  default from: ENV['CB_MAIL_FR']
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.inquiry_notifier.create.subject
-  #
   def create(inquiry)
     @inquiry = inquiry
-
-    mail :to => ENV['CB_MAIL_TO'], :subject => "Webサイトからの問い合わせ [" + inquiry.name + "]"
+    mail to: ENV['CB_MAIL_TO'], subject: "Webサイトからの問い合わせ [#{inquiry.name}]"
   end
 end
