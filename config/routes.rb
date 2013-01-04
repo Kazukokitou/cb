2a139c98cd283b9714f645410fcdc03bc100eef3
Junichi Ito <jit@sonicgarden.jp>
2013-01-05 05:55:41 +0900

    Refine inquiry routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Cb::Application.routes.draw do
-  resources :inquiries, only: %w(create new)
-
+  root to: "homes#show"
   resource :concept, only: :show
   resource :menu, only: :show
   resource :shop_info, only: :show
-
-  root to: "homes#show"
+  resource :inquiry, only: %w(create new)
 
   # Keep old paths
   match "pages/concept" => redirect('/concept')
   match "pages/menu" => redirect('/menu')
   match "pages/shop_info" => redirect('/shop_info')
+  match "inquiries/new" => redirect('/inquiry/new')
 end
