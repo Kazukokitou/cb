22b669de59923984ccda3044fa49ccbc9bbc9e85
Junichi Ito <jit@sonicgarden.jp>
2013-01-04 22:14:27 +0900

    Arrange FB icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 # coding: utf-8
 module ApplicationHelper
-  def fb_link
-    "☞ #{fb_icon}".html_safe
+  def fb_text_icon
+    "#{fb_text} #{fb_icon}".html_safe
+  end
+
+  def fb_text
+    link_to "Facebookページ", fb_url, target: '_blank'
   end
 
   def fb_icon
-    link_to image_tag('fb.png'),  "http://www.facebook.com/coupebaguette", target: '_blank', class: 'fb-icon'
+    link_to image_tag('fb.png'), fb_url, target: '_blank', class: 'fb-icon'
+  end
+
+  def fb_url
+    "http://www.facebook.com/coupebaguette"
   end
 end
