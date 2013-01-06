ee4b3d3fac62d1af5c85cab8a723c24474c01494
Junichi Ito <jit@sonicgarden.jp>
2013-01-07 04:34:56 +0900

    Refactoring

--- app/models/blog_info.rb
+++ app/models/blog_info.rb
@@ -1,26 +1,39 @@
 class BlogInfo < ActiveRecord::Base
-  attr_accessible :published_at, :title, :url
+  RSS_URL = "http://rss.exblog.jp/rss/exblog/lapin418/atom.xml" 
 
   def self.update_info
     logger.info "####### Start update_info"
-    source = "http://rss.exblog.jp/rss/exblog/lapin418/atom.xml" 
-    feed = FeedNormalizer::FeedNormalizer.parse(open(source), force_parser: FeedNormalizer::SimpleRssParser).items[0] 
+
+    feed = fetch_rss_feed
 
     self.transaction do
-      if self.count == 1
-        blog_info = self.first
-      else
-        self.destroy_all
-        blog_info = self.new
-      end
+      info = item_for_update
+      info.title = feed[:title]
+      info.published_at = feed[:published_at]
+      info.url = feed[:url]
 
-      blog_info.title = feed.title.force_encoding('utf-8')
-      blog_info.published_at = feed.date_published
-      blog_info.url = feed.urls[0]
-
-      logger.info "Save: #{blog_info.inspect}"
-      blog_info.save!
+      logger.info "Save: #{info.inspect}"
+      info.save!
     end
+
     logger.info "####### Complete update_info"
+  end
+
+  def self.item_for_update
+    if self.count == 1
+      self.first
+    else
+      self.destroy_all
+      self.new
+    end
+  end
+
+  def self.fetch_rss_feed
+    feed = FeedNormalizer::FeedNormalizer.parse(open(RSS_URL), force_parser: FeedNormalizer::SimpleRssParser).items[0] 
+    {
+      title: feed.title.force_encoding('utf-8'),
+      published_at: feed.date_published,
+      url: feed.urls[0]
+    }
   end
 end
