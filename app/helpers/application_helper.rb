3f9087aa60b62f1a8f1427ba6a15c28b066265a6
Junichi Ito <jit@sonicgarden.jp>
2013-01-07 04:36:57 +0900

    Refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,16 @@
 # coding: utf-8
 module ApplicationHelper
+  FACEBOOK_PAGE_URL = "http://www.facebook.com/coupebaguette"
+
   def fb_text_icon
     "#{fb_text} #{fb_icon}".html_safe
   end
 
   def fb_text
-    link_to "Facebookページ", fb_url, target: '_blank'
+    link_to "Facebookページ", FACEBOOK_PAGE_URL, target: '_blank'
   end
 
   def fb_icon
-    link_to image_tag('fb.png'), fb_url, target: '_blank', class: 'fb-icon'
-  end
-
-  def fb_url
-    "http://www.facebook.com/coupebaguette"
+    link_to image_tag('fb.png'), FACEBOOK_PAGE_URL, target: '_blank', class: 'fb-icon'
   end
 end
