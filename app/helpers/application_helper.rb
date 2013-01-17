47f398a03dc3b726491bf231fc559d116d5a090b
Junichi Ito <jit@sonicgarden.jp>
2013-01-17 17:04:17 +0900

    Arrange FB link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,9 @@ module ApplicationHelper
   FACEBOOK_PAGE_URL = "http://www.facebook.com/coupebaguette"
 
   def fb_text_icon
-    "#{fb_text} #{fb_icon}".html_safe
-  end
-
-  def fb_text
-    link_to "Facebookページ", FACEBOOK_PAGE_URL, target: '_blank'
+    link_to FACEBOOK_PAGE_URL, target: '_blank', class: 'fb-icon' do
+      "Facebookページ #{image_tag 'fb.png'}".html_safe
+    end
   end
 
   def fb_icon
